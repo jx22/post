0f71005650b24d941740fe5a90fec12c928932a9
jx22 <thesnowmountains@gmail.com>
2017-04-23 21:36:31 -0700

    Add to firebase call

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var submitBtn = document.getElementById("submitBtn");
 
 function submitClick() {
 
-    var firebaseRef = firebase.database().ref()
-    firebaseRef.child("Text").set("SomeValue"); 
+    var firebaseRef = firebase.database().ref();
+    var messageText = mainText.value;
+    firebaseRef.child("Text").set(messageText); 
 
 
 }
